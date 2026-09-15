a62f12001bbd5579a56619ce838ebee469da5d14
AVANISH SINGH <singhavanish8930@gmail.com>
2026-09-15 21:18:30 +0530

    views created

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
 router.get("/",function(req,res) {
     res.send("I am the owner route and working properly");
 });
+
+   router.post("/create", async function(req,res){
+       let owners = await ownerModel.find();
+       if(owners.length>0) {
+        return res
+         .status(403)
+         .send("you dont have permission to create a new owner");
+       }
+          let { fullname,email,password,gstin } = req.body;
+          let createdOwner = await ownerModel.create({
+             fullname,
+             email,
+            password,
+            gstin,
+          });
+           res.status(201).send(createdOwner);
+    });
+
 
 module.exports = router;
